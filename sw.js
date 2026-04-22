5bfed4e2db2351fee29431ed7173a8a80bef4755
thorsten1708 <thorsten1708@users.noreply.github.com>
2026-04-22 16:02:56 +0200

    Initial commit: Meeting@FlexWork 2026 App

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'mfw2026-v1';
+const ASSETS = [
+  '/flexwork2026/',
+  '/flexwork2026/index.html',
+  '/flexwork2026/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        return caches.open(CACHE).then(cache => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
